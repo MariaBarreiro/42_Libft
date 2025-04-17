e8b3813b0748f6937abed427be17560a1054169f
Maria Barreiro <mariaeobarreiro@gmail.com>
2025-04-17 17:33:10 +0100

    Memset Revised

--- ft_memset.c
+++ ft_memset.c
@@ -36,12 +36,59 @@ void	*ft_memset(void *s, int c, size_t n)
 #include <string.h>
 #include <stdio.h>
 
-int	main()
+int	main(void)
 {
-	char str[] = "Hello World";
-	ft_memset(str, 'H', 5);
-	memset(str, 'H', 5);
-	printf("ft_memset: %s\n", str);
-	printf("memset: %s", str);
-	return (0);
+	char cases[][32] = {
+		"Hello World!",
+		"abcdef",
+		"1234567890",
+		"with\0nulls",     // test with embedded null
+		"short",
+		"",
+	};
+
+	int chars[] = {
+		'X',
+		'Z',
+		'0',
+		'@',
+		'A',
+		'#'
+	};
+
+	size_t sizes[] = {
+		5,   // partial overwrite
+		6,   // full string
+		10,  // numbers
+		9,   // includes null
+		3,   // partial
+		0    // nothing to overwrite
+	};
+
+	int count = sizeof(sizes) / sizeof(sizes[0]);
+	int i = 0;
+
+	while (i < count)
+	{
+		char std_buf[32];
+		char ft_buf[32];
+
+		strcpy(std_buf, cases[i]);
+		strcpy(ft_buf, cases[i]);
+
+		memset(std_buf, chars[i], sizes[i]);
+		ft_memset(ft_buf, chars[i], sizes[i]);
+
+		printf("  memset result   : \"%s\"\n", std_buf);
+		printf("  ft_memset result: \"%s\"\n", ft_buf);
+
+		if (memcmp(std_buf, ft_buf, sizes[i]) == 0)
+			printf("MATCH\n");
+		else
+			printf("MISMATCH\n");
+
+		printf("--------------------------\n");
+		i++;
+	}
+	return 0;
 }*/
