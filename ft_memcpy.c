1ba737634cd64e6b70e966d65b806b9bab0608e0
Maria Barreiro <mariaeobarreiro@gmail.com>
2025-04-17 17:30:50 +0100

    Memcpy Revised

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -37,18 +37,55 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
+
 /*
 #include <string.h>
 #include <stdio.h>
 
-int	main()
+int	main(void)
 {
-	unsigned char d[] = "Help please";
-	unsigned char s[] = "Hello World";
-	printf("dest: %s\nsrc: %s\n", d, s);
-	ft_memcpy(d, s, 5);
-	memcpy(d, s, 5);
-	printf("ft_memcpy: %s\n",d);
-	printf("memcpy: %s", d);
-	return (0);
+	char cases[][32] = {
+		"Hello, World!",
+		"abcdef",
+		"1234567890",
+		"with\0nulls",
+		"overlap test",   // but memcpy shouldn't handle overlaps!
+		"short",
+		"",
+	};
+
+	size_t sizes[] = {
+		5,    // Copy part of string
+		6,    // Copy full string
+		10,   // Copy digits
+		9,    // Includes null byte in middle
+		7,    // Test with potential overlap
+		3,    // Partial copy
+		0     // Nothing to copy
+	};
+
+	int count = sizeof(sizes) / sizeof(sizes[0]);
+	int i = 0;
+
+	while (i < count)
+	{
+		char std_buf[32] = {0};
+		char ft_buf[32] = {0};
+
+		memcpy(std_buf, cases[i], sizes[i]);
+		ft_memcpy(ft_buf, cases[i], sizes[i]);
+
+		printf("  memcpy result   : \"%s\"\n", std_buf);
+		printf("  ft_memcpy result: \"%s\"\n", ft_buf);
+
+		if (memcmp(std_buf, ft_buf, sizes[i]) == 0)
+			printf("MATCH\n");
+		else
+			printf("MISMATCH\n");
+
+		printf("--------------------------\n");
+		i++;
+	}
+
+	return 0;
 }*/
