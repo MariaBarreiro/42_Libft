4e8e5e4f095232255e5f8a13e421fef856cc6816
Maria Barreiro <mariaeobarreiro@gmail.com>
2025-04-18 08:57:43 +0100

    Striteri Revised

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: mda-enca <mda-enca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 17:39:11 by mda-enca          #+#    #+#             */
-/*   Updated: 2025/04/15 18:17:21 by mda-enca         ###   ########.fr       */
+/*   Updated: 2025/04/18 08:57:14 by mda-enca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,34 @@ void	ft_to_upper(unsigned int i, char *c)
 
 #include <stdio.h>
 
-int	main()
+int	main(void)
 {
-	char s[] = "abcd";
-	ft_striteri(s, ft_to_upper);
-	printf("ft_striteri: %s\n", s); 
-	return (0);
+	char str1[] = "hello world";
+	char str2[] = "";
+	char *null_str = NULL;
+
+	// Test 1: Normal lowercase string
+	printf("Before ft_striteri: %s\n", str1);
+	ft_striteri(str1, ft_to_upper);
+	printf("After  ft_striteri: %s\n", str1);
+	printf("----------------------------------\n");
+
+	// Test 2: Empty string
+	printf("Before ft_striteri (empty): \"%s\"\n", str2);
+	ft_striteri(str2, ft_to_upper);
+	printf("After  ft_striteri (empty): \"%s\"\n", str2);
+	printf("----------------------------------\n");
+
+	// Test 3: NULL string (should do nothing)
+	printf("Test with NULL string:\n");
+	ft_striteri(null_str, ft_to_upper);
+	printf("No crash\n");
+	printf("----------------------------------\n");
+
+	// Test 4: NULL function (should do nothing)
+	printf("Test with NULL function:\n");
+	ft_striteri(str1, NULL);
+	printf("No crash\n");
+
+	return 0;
 }*/
