7e44d341cbbc37164f6a9b0b728cc3ffaf1c3f13
Maria Barreiro <mariaeobarreiro@gmail.com>
2025-04-21 07:37:47 +0100

    libft.h Norm corrected

--- libft.h
+++ libft.h
@@ -13,9 +13,9 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stddef.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
